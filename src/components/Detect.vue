9ef3d05e21781e6af01f37f4abf4d96cbcb9e780
smh0505 <mson2017@my.fit.edu>
2023-09-13 09:38:06 -0400

    Details updated

--- src/components/Detect.vue
+++ src/components/Detect.vue
@@ -4,7 +4,9 @@
             <div @click="clear()"><span class="material-symbols-outlined">delete</span></div>
             <div @click="paste()"><span class="material-symbols-outlined">content_paste</span></div>
             <input type="text" placeholder="output (.json)" v-model="filename">
-            <div @click="save()" v-if="input && regex"><span class="material-symbols-outlined">save</span></div>
+            <div @click="save()" :class="{ enabled: input && regex }">
+                <span class="material-symbols-outlined">save</span>
+            </div>
         </div>
         <input type="text" :placeholder="texts.detect.pattern" v-model="regex">
         <div id="detect-backlayer" ref="highlight"><div v-html="result"></div></div>
@@ -61,22 +63,24 @@ export default {
         clear() { this.input = "" },
         paste() { navigator.clipboard.readText().then((text: string) => (this.input = text)) },
         save() {
-            const output = {
-                input: this.input,
-                pattern: this.regex,
-                result: this.matched()
+            if (this.input && this.regex) {
+                const output = {
+                    input: this.input,
+                    pattern: this.regex,
+                    result: this.matched()
+                }
+                let filename = filenamify(this.filename.trim())
+                if (!filename) filename = 'output.json'
+
+                const url = URL.createObjectURL(new Blob([JSON.stringify(output, null, 4)], { type: 'application/json' }))
+                const link = document.createElement('a')
+                link.href = url
+                link.download = filename.match(/.json$/i) ? filename : filename + '.json'
+
+                document.body.appendChild(link)
+                link.click()
+                document.body.removeChild(link)
             }
-            let filename = filenamify(this.filename.trim())
-            if (!filename) filename = 'output.json'
-
-            const url = URL.createObjectURL(new Blob([JSON.stringify(output, null, 4)], { type: 'application/json' }))
-            const link = document.createElement('a')
-            link.href = url
-            link.download = filename.match(/.json$/i) ? filename : filename + '.json'
-
-            document.body.appendChild(link)
-            link.click()
-            document.body.removeChild(link)
         }
     }
 }
@@ -92,23 +96,42 @@ export default {
     gap: 8px;
 
     #detect-header {
-        display: grid;
-        grid-template-columns: 40px 40px 1fr 40px;
+        display: flex;
         gap: 4px;
         
-        >:nth-child(1), >:nth-child(2), >:nth-child(4) {
+        >:nth-child(1), >:nth-child(2) {
             @include convert-buttons;
             margin: 0px;
+            height: 32px;
         }
 
-        >:nth-child(3) { padding: 0px 8px; }
+        >:nth-child(3) {
+            padding: 0px 8px;
+            width: 100%;
+        }
+
+        >:nth-child(4) {
+            display: flex;
+            align-items: center;
+            padding-inline: 6px;
+
+            color: var(--disabled-color);
+            font: normal 24px sans-serif;
+            user-select: none;
+            
+            &.enabled {
+                @include convert-buttons;
+                margin: 0px;
+                height: 32px;
+            }
+        }
     }
 
     input { @include input; }
 
     #detect-backlayer {
         position: absolute;
-        top: 98px;
+        top: 90px;
         width: 100%;
         height: 120px;
         border: 4px solid transparent;
@@ -123,7 +146,7 @@ export default {
             white-space: pre-wrap;
             word-wrap: break-word;
             color: transparent;
-            font: 20px "D2Coding", sans-serif;
+            font: 16px "D2Coding", sans-serif;
 
             span {
                 background-color: var(--highlight-color);
@@ -136,7 +159,6 @@ export default {
         height: 120px;
         @include input;
         background-color: transparent;
-        font: 20px "D2Coding", sans-serif;
         z-index: 5;
     }
 
@@ -150,14 +172,14 @@ export default {
             margin-bottom: 4px;
             
             >:nth-child(1) {
-                font: 20px "D2Coding", sans-serif;
+                font: 16px "D2Coding", sans-serif;
                 background-color: var(--highlight-color);
                 color: var(--color);
                 @include trans;
             }
             
             >:nth-child(2) {
-                font: 12px sans-serif;
+                font: 12px "D2Coding", sans-serif;
                 color: var(--color);
                 @include trans;
             }
